b71be4206e632fe81b0d676024eb1f7688c4e0fa
Mauricio Leal <mauricio@macmaltron.local>
2014-03-17 14:08:45 -0300

    Development of iOS Client

---
+++ client/iOS/saruman/saruman/Role.m
@@ -0,0 +1,19 @@
+//
+//  Role.m
+//  saruman
+//
+//  Created by Mauricio Leal on 3/4/14.
+//  Copyright (c) 2014 Mauricio "Maltron" Leal. All rights reserved.
+//
+
+#import "Role.h"
+#import "User.h"
+
+
+@implementation Role
+
+@dynamic id;
+@dynamic name;
+@dynamic user;
+
+@end
