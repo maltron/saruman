7a0fadc4a773f6e4ea43bac2256aa30f39e12cde
Mauricio Leal <maltron@gmail.com>
2014-03-19 21:57:54 -0300

    New Development on the iOS Client side

---
+++ client/iOS (Case Conflict)/saruman/saruman/User.m
@@ -0,0 +1,22 @@
+//
+//  User.m
+//  saruman
+//
+//  Created by Mauricio Leal on 3/4/14.
+//  Copyright (c) 2014 Mauricio "Maltron" Leal. All rights reserved.
+//
+
+#import "User.h"
+#import "Role.h"
+
+
+@implementation User
+
+@dynamic enabled;
+@dynamic fullName;
+@dynamic id;
+@dynamic password;
+@dynamic username;
+@dynamic role;
+
+@end
